be4fe9b3791de9b82b139a7b3d63944353694095
jandi-mei <jjeon4647@gmail.com>
2024-08-24 02:00:43 -0700

    initial commit

---
+++ catkin_ws/build/ball_chaser/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/workspace/Project4/Map_My_World/catkin_ws/src/ball_chaser
+# Build directory: /home/robond/workspace/Project4/Map_My_World/catkin_ws/build/ball_chaser
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/pgm_map_creator/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/robond/workspace/Project4/Map_My_World/catkin_ws/src/pgm_map_creator
+# Build directory: /home/robond/workspace/Project4/Map_My_World/catkin_ws/build/pgm_map_creator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(msgs)
